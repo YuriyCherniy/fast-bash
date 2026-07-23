b7cadf2f6aae7be7a103dc106764125e09c0baff
Yuriy Cherniy <YuriyCherniyMail@yandex.ru>
2026-07-23 16:03:29 +0300

    fixed passanger_wsgy.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,18 +1,7 @@
 import os, sys
-from pathlib import Path
-
-import environ
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-env = environ.Env()
-env_file = os.path.join(BASE_DIR, '.env')
-environ.Env.read_env(env_file)
-
-USER = env('USER')
-
+USER = os.getenv('USER')
 
 sys.path.insert(0, f'/var/www/{USER}/data/www/faq-reg.ru/project_name')
 sys.path.insert(1, f'/var/www/{USER}/data/.venv/lib/python3.12/site-packages')
