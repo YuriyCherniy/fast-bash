1a23b61465fcfdf61e195f0e222a78f288ccb4b8
Yuriy Cherniy <YuriyCherniyMail@yandex.ru>
2026-09-13 23:19:10 +0300

    minor changes

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,4 +1,7 @@
-import os, sys
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
 
 
 USER = os.getenv('USER')
@@ -6,5 +9,5 @@ USER = os.getenv('USER')
 sys.path.insert(0, f'/var/www/{USER}/data/www/faq-reg.ru/project_name')
 sys.path.insert(1, f'/var/www/{USER}/data/.venv/lib/python3.12/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'fast_bash.settings'
-from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
